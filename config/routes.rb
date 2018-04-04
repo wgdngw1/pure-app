b9e5bd537f47295e140bb45de0639be29b03c20f
ren <ren@sugikawarennoMacBook-Air.local>
2018-04-04 18:15:23 +0900

    show_link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'movies#index'
   get 'movies/index'
-
-  get 'movies/show'
-
+  get '/movies/:id', to: 'movies#show', as: 'movies_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
